a1607460186136a37cc24644871467ecbcd15708
Connor Mulholland <connor.mulholland@hpe.com>
2016-03-11 00:00:12 +0000

    CAF-605: code review changes

--- src/main/resources/getJob.sql
+++ src/main/resources/getJob.sql
@@ -4,7 +4,7 @@
  *  Description:  Returns the job definition for the specified job.
  */
 CREATE OR REPLACE FUNCTION get_job(in_job_id VARCHAR(48))
-  RETURNS TABLE ( job_id VARCHAR(48), name VARCHAR(255), description TEXT, data TEXT, create_date TIMESTAMP, status job_status, percentage_complete double precision, failure_details TEXT, actionType CHAR(6)) AS $$
+  RETURNS TABLE ( job_id VARCHAR(48), name VARCHAR(255), description TEXT, data TEXT, create_date TEXT, status job_status, percentage_complete double precision, failure_details TEXT, actionType CHAR(6)) AS $$
 BEGIN
   --  Raise exception if the job identifier has not been specified.
   IF in_job_id IS NULL OR in_job_id = '' THEN
@@ -14,7 +14,7 @@ BEGIN
   --  Return job metadata belonging to the specified job_id.
   --  'WORKER' is the only supported action type for now.
   RETURN QUERY
-  SELECT job.job_id, job.name, job.description, job.data, job.create_date, job.status, job.percentage_complete, job.failure_details, CAST('WORKER' AS CHAR(6)) AS actionType
+  SELECT job.job_id, job.name, job.description, job.data, to_char(job.create_date, 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"'), job.status, job.percentage_complete, job.failure_details, CAST('WORKER' AS CHAR(6)) AS actionType
   FROM job WHERE job.job_id = in_job_id;
 
   IF NOT FOUND THEN
