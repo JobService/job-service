1cde1b2110e1cd9e9bde90c9d0c2f85533855332
Philip Crooks <philip.crooks@hpe.com>
2016-04-13 17:49:37 +0100

    CAF-598: Moving message forwarding to the worker factory.

--- src/main/java/com/hpe/caf/worker/jobtracking/JobTrackingWorkerTask.java
+++ src/main/java/com/hpe/caf/worker/jobtracking/JobTrackingWorkerTask.java
@@ -2,24 +2,18 @@ package com.hpe.caf.worker.jobtracking;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.util.Objects;
 
 /**
  * This task is used to inform the Job Tracking worker of the progress of another task.
- * EITHER jobTaskId OR proxiedTaskInfo should have a value.
  */
 public class JobTrackingWorkerTask {
     /**
      * The jobTaskId of the tracked task whose progress is being reported.
-     * If this has a value then proxiedTaskInfo should be null.
      */
+    @NotNull
     private String jobTaskId;
-
-    /**
-     * Details of a proxied task.
-     * If this has a value then jobTaskId should be null.
-     */
-    private ProxiedTaskInfo proxiedTaskInfo;
 
 
     @Min(0)
@@ -34,19 +28,8 @@ public class JobTrackingWorkerTask {
 
     public void setJobTaskId(String jobTaskId) {
         this.jobTaskId = Objects.requireNonNull(jobTaskId);
-        this.proxiedTaskInfo = null;
     }
 
-
-    public ProxiedTaskInfo getProxiedTaskInfo() {
-        return proxiedTaskInfo;
-    }
-
-
-    public void setProxiedTaskInfo(ProxiedTaskInfo proxiedTaskInfo) {
-        this.proxiedTaskInfo = proxiedTaskInfo;
-        this.jobTaskId = null;
-    }
 
     public int getEstimatedPercentageCompleted() {
         return estimatedPercentageCompleted;
