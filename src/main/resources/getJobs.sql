d303b3a932df028d58ac8ce69c53014c8c661e6f
Zara McKeown <zara.mckeown@hpe.com>
2016-11-17 18:46:42 +0000

    CAF-1748: Updated to return just the first 25 jobs (#3)

--- src/main/resources/getJobs.sql
+++ src/main/resources/getJobs.sql
@@ -11,7 +11,7 @@ BEGIN
   -- 'WORKER' is the only supported action type for now and this is returned.
   RETURN QUERY
   SELECT job.job_id, job.name, job.description, job.data, to_char(job.create_date, 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"'), job.status, job.percentage_complete, job.failure_details, CAST('WORKER' AS CHAR(6)) AS actionType
-  FROM job;
+  FROM job LIMIT 25;
 
 END
 $$ LANGUAGE plpgsql;
