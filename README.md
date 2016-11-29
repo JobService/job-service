a057f1274794e2467fdf0af8ff91fa335e6c4f19
Connor Mulholland <connor.mulholland@hpe.com>
2016-11-29 22:56:57 +0000

    CAF-1883: Added python script to create jobs and wait on them to finish (#2)

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
 # job-service-caller
-A script to call the Job Service which creates a new job and waits on job completion or failure.
+A script to call the Job Service which creates a new job and waits on job completion or failure. The script requires a job identifier, a
+job definition and the web service url for the CAF Job Service.
+
+## Usage
+
+The CAF Job Service should be deployed prior to running the script.
+See the [Getting Started](https://pages.github.hpe.com/caf/job-service/pages/en-us/Getting-Started) guide for this.
+
+The script can be invoked using Python:
+
+    python createJob.py [-h] -j jobId -u jobWebServiceURL [-c correlationId] [-f jobDefinitionFilename] [-p pollingInterval]
+
+where:
+
+- **-j** or **--job jobId** (required)
+    - specifies the job identifier.
+- **-u** or **--url jobWebServiceURL** (required)
+    - specifies the CAF Job Service URL.
+- **-h** or **--help** (optional) 
+    - used to print a short description of all command line options.
+- **-c** or **--correlation correlationId** (optional) 
+    - specifies the correlation identifier.
+- **-f** or **--filename jobDefinitionFilename** (optional) 
+    - specifies the file name comprising the definition of the job to create.
+      If the file name is not specified, the job definition will be read from stdin.
+- **-p** or **--polling pollingInterval** (optional) 
+    - specifies the polling interval in seconds used to wait on job completion.
+
+### Display Help
+
+Run the following to print a short description of the command line options:
+
+    python createJob.py -h
+
+Output:
+
+    usage: createJob.py [-h] -j jobId -u jobWebServiceURL [-c correlationId] [-f jobDefinitionFilename] [-p pollingInterval]
+    
+    Create a new job.
+    
+    required arguments:
+      -j jobId, --job jobId                                             job identifier
+      -u jobWebServiceURL, --url jobWebServiceURL                       job web service url
+    
+    optional arguments:
+      -h, --help                                                        show this help message and exit
+      -c correlationId, --correlation correlationId                     correlation identifier
+      -f jobDefinitionFilename, --filename jobDefinitionFilename        file name comprising the definition of the job to create
+      -p pollingInterval, --polling pollingInterval                     polling interval
+
+
+
+### Create a New Job
+
+Sample usage instructions for creating a new job are provided next.
+
+#### Create Job Definition JSON File
+
+A JSON file (e.g. MyJobDefinition.json) comprising the job definition should be generated in the directory containing the job creation
+script. A sample JSON has been provided next.
+
+    {
+        "name": "TestJob",
+        "description": "Test description for end-to-end test job.",
+        "externalData": "string",
+        "task": {
+            "taskClassifier": "BatchWorker",
+            "taskApiVersion": 1,
+            "taskData": "{\"batchDefinition\":\"[\\\"b591d8c6615c4af99d7915719b01259c/3a44156891e645c6828cfe47667f159f\\\"]\",\"batchType\":\"AssetIdBatchPlugin\",\"taskMessageType\":\"ExampleWorkerTaskBuilder\",\"taskMessageParams\":{\"datastorePartialReference\":\"b591d8c6615c4af99d7915719b01259c\",\"action\":\"REVERSE\"},\"targetPipe\":\"dataprocessing-example-in\"}",
+            "taskDataEncoding": "utf8",
+            "taskPipe": "dataprocessing-batch-in",
+            "targetPipe": "dataprocessing-example-out"
+        }
+    }
+
+
+#### Run Script
+
+##### Job Definition by File
+The JSON file created above can be used to create a new job by running the following:
+
+    python createJob.py -j TestJob_1 -u http://vagrant-mesos:9410 -f MyJobDefinition.json
+
+where:
+
+- `TestJob` is the job identifier.
+- `http://vagrant-mesos:9410` is the CAF Job Web Service URL.
+- `MyJobDefinition.json` is the name of the file in the current directory comprising a job definition.
+
+##### Job Definition by Standard Input
+The job definition can also be provided via stdin by running the following:
+
+    cat MyJobDefinition.json | python createJob.py -j TestJob_2 -u http://vagrant-mesos:9410
---
+++ script/createJob.py
@@ -0,0 +1,160 @@
+#!/usr/bin/python
+#
+# ******************************************************************************
+# * Script to create a new job using the CAF Job Service. The script continues
+# * to poll the CAF Job Service until the job has completed or failed.
+# *****************************************************************************
+#
+# usage: createJob.py [-h] -j jobId -u jobWebServiceURL [-c correlationId] [-f jobDefinitionFilename] [-p pollingInterval]
+
+import argparse                 # for command line parsing
+from datetime import datetime   # for printing date and time as part of logging
+import json                     # for JSON parsing and validation
+import requests                 # for calling the CAF Job Service
+import sys
+import time
+
+# CAF Job Service endpoint.
+job_service_api_endpoint = '/job-service/v1/jobs/'
+
+# Global job definition.
+job_definition = ''
+
+# Log message to standard output.
+def log(message):
+    print (datetime.now(), message)
+
+def successful_termination():
+    sys.exit()
+
+def abnormal_termination():
+    sys.exit(1)
+
+# Parse and validate command line arguments.
+def parse_args():
+    parser = argparse.ArgumentParser(description='Create a new job.')
+
+    # Required arguments.
+    required_named = parser.add_argument_group('required arguments')
+    required_named.add_argument('-j', '--job', dest='jobId', help='job identifier', required=True, metavar='jobId')
+    required_named.add_argument('-u', '--url', dest='jobWebServiceURL', help='job web service url', required=True, metavar='jobWebServiceURL')
+
+    # Optional arguments.
+    optional = parser.add_argument_group('optional arguments')
+    optional.add_argument('-c', '--correlation', dest='correlationId', default='', help='correlation identifier', metavar='correlationId')
+    optional.add_argument('-f', '--filename', dest='jobDefinitionFilename', type=argparse.FileType('r'), default=sys.stdin, help='file name comprising the definition of the job to create', metavar='jobDefinitionFilename')
+    optional.add_argument('-p', '--polling', dest='pollingInterval', default=30, type=int, help='polling interval', metavar='pollingInterval')
+
+    args = parser.parse_args()
+
+    # Validate input arguments.
+    validate_args(args)
+
+    return args
+
+# Attempt to validate input JSON.
+def is_valid_json(json_to_validate):
+    try:
+        json.loads(json_to_validate)
+    except ValueError:
+        return False
+    return True
+
+# Validate command line arguments.
+def validate_args(args):
+    log('Validating command line arguments ...')
+
+    # Make sure input arguments are not empty.
+    if args.jobId == "":
+        log('The job identifier argument is empty. Exiting.')
+        abnormal_termination()
+
+    if args.jobDefinitionFilename is not None:
+        # Make sure the supplied job definition is valid JSON.
+        try:
+            global job_definition
+            job_definition = json.dumps(json.load(args.jobDefinitionFilename))
+
+            if not is_valid_json(job_definition):
+                log('The job definition provided is not valid JSON. Exiting.')
+                abnormal_termination()
+        except IOError:
+            log('Error reading job definition file. Exiting.')
+            abnormal_termination()
+    else:
+        log('The job definition has not been provided. Exiting.')
+        abnormal_termination()
+
+    if args.jobWebServiceURL == "":
+        log('The job web service url argument is empty. Exiting.')
+        abnormal_termination()
+
+# Create a new job via the CAF Job Service.
+def call_put_job_service(uri, job_definition):
+    log('Calling CAF Job Service to create a new job with job definition %s ...' % job_definition)
+
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+
+    # Issue call to the CAF Job Service to create a new job using the specified job definition.
+    put_response = requests.put(uri, data=job_definition, headers=headers)
+
+    # For successful API call, response code should be 200 (OK).
+    if not put_response.ok:
+        log('Job creation has failed with %s ...' % put_response.raise_for_status())
+        abnormal_termination()
+
+# Retrieve job metadata via the CAF Job Service including status.
+def call_get_job_service(uri):
+    log('Calling CAF Job Service to retrieve job status ...')
+
+    # Issue call to the CAF Job Service to retrieve the job metadata.
+    headers = {'Accept': 'application/json'}
+    get_response = requests.get(uri, headers=headers)
+
+    # For successful API call, response code should be 200 (OK).
+    if get_response.ok:
+        # Load the job metadata into a dict variable and return.
+        json_object = json.loads(get_response.content)
+        return json_object
+    else:
+        log(get_response.raise_for_status())
+        abnormal_termination()
+
+# Continue to poll the CAF Job Service until job has completed or failed.
+def poll_job_service(uri, polling_interval):
+    log('Polling CAF Job Service awaiting job completion ...')
+
+    is_completed = False
+    while not is_completed:
+        job_response = call_get_job_service(uri)
+
+        if job_response["status"] == 'Completed' or job_response["status"] == 'Cancelled':
+            # Job has completed.
+            is_completed = True
+            log('Job creation completed.')
+            successful_termination()
+        elif job_response["status"] == 'Failed':
+            # Job has failed.
+            log('Job creation failed ...')
+            abnormal_termination()
+        else:
+            # Job is still in active state. Go to sleep and retry.
+            log('Going to sleep for ' + str(polling_interval) + ' seconds awaiting job completion ...')
+            time.sleep(polling_interval)
+
+def main():
+    # Validate and parse command line arguments.
+    args = parse_args()
+
+    # Construct uri to be used in calls to the CAF Job Service.
+    uri = args.jobWebServiceURL + job_service_api_endpoint + args.jobId
+
+    # Call CAF Job Service to create a new job.
+    call_put_job_service(uri, job_definition)
+
+    # Poll CAF Job Service awaiting completion of the newly created job.
+    poll_job_service(uri, args.pollingInterval)
+
+if __name__ == '__main__':
+    # Only want to run this as the main file.
+    main()
