7ce8c5a95a021abb086137bc188f36a07b77e656
Jason Albert <jason.albert@hpe.com>
2016-11-30 12:28:14 -0500

    [CAF-1826] [CAF-1838] [CAF-1909] Change statusType to enum (#7)

--- src/main/resources/getJobs.sql
+++ src/main/resources/getJobs.sql
@@ -3,34 +3,37 @@
  *
  *  Description:  Returns the list of job definitions in the system.
  */
-CREATE OR REPLACE FUNCTION get_jobs(in_job_id_starts_with VARCHAR(48), in_status_type INT, in_limit INT, in_offset INT)
+CREATE OR REPLACE FUNCTION get_jobs(in_job_id_starts_with VARCHAR(48), in_status_type VARCHAR(20), in_limit INT, in_offset INT)
   RETURNS TABLE ( job_id VARCHAR(48), name VARCHAR(255), description TEXT, data TEXT, create_date TEXT, status job_status, percentage_complete double precision, failure_details TEXT, actionType CHAR(6)) AS $$
 DECLARE
   sql VARCHAR;
+  escapedJobIdStartsWith VARCHAR;
   whereOrAnd VARCHAR(7) = ' WHERE ';
   andConst CONSTANT VARCHAR(5) = ' AND ';
 BEGIN
 
   -- Return all rows from the job table:
   --   If the in_job_id param is specified, only those rows starting with that param will be returned.
-  --   If the in_status_type param is 1, only those rows with statuses other than 'Completed' will be returned. If 2, only those rows with 'Completed' status will be returned. If 3, only those rows with inactive statuses will be returned. Anything else returns all statuses.
+  --   If the in_status_type param is NotCompleted - only those results with statuses other than Completed will be returned; Completed - only those results with Completed status will be returned; Inactive - only those results with inactive statuses (i.e. Completed, Failed, Cancelled) will be returned; Anything else returns all statuses.
   -- Also accepts in_limit and in_offset params to support paging and limiting the number of rows returned.
   -- 'WORKER' is the only supported action type for now and this is returned.
   sql := $q$SELECT job.job_id, job.name, job.description, job.data, to_char(job.create_date, 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"'), job.status, job.percentage_complete, job.failure_details, CAST('WORKER' AS CHAR(6)) AS actionType FROM job$q$;
 
   IF in_job_id_starts_with IS NOT NULL AND in_job_id_starts_with != '' THEN
-    sql := sql || whereOrAnd || ' job_id LIKE ' || quote_literal(in_job_id_starts_with || '%');
+    escapedJobIdStartsWith = replace(replace(quote_literal(in_job_id_starts_with), '_', '\_'), '%', '\%');
+    escapedJobIdStartsWith = left(escapedJobIdStartsWith, char_length(escapedJobIdStartsWith) - 1) || $q$%'$q$;
+    sql := sql || whereOrAnd || ' job_id LIKE ' || escapedJobIdStartsWith;
     whereOrAnd := andConst;
   END IF;
 
   IF in_status_type IS NOT NULL THEN
-    IF in_status_type = 1 THEN
+    IF in_status_type = 'NotCompleted' THEN
       sql := sql || whereOrAnd || $q$ status IN ('Active','Paused','Waiting','Cancelled','Failed')$q$;
       whereOrAnd := andConst;
-    ELSIF in_status_type = 2 THEN
+    ELSIF in_status_type = 'Completed' THEN
       sql := sql || whereOrAnd || $q$ status IN ('Completed')$q$;
       whereOrAnd := andConst;
-    ELSIF in_status_type = 3 THEN
+    ELSIF in_status_type = 'Inactive' THEN
       sql := sql || whereOrAnd || $q$ status IN ('Completed','Cancelled','Failed')$q$;
       whereOrAnd := andConst;
     END IF;
