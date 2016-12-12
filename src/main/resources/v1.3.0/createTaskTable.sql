404ef62f1107af5b0b5f4ab46125e6bd236940fb
Zara McKeown <zara.mckeown@hpe.com>
2016-12-12 12:53:19 +0000

    CAF-1830: Fixing changelog.xml upgrade issue (#9)

---
+++ src/main/resources/v1.3.0/createTaskTable.sql
@@ -0,0 +1,43 @@
+/*
+ *  Name: internal_create_task_table
+ *
+ *  Description:  Create a new task table when the first sub-task is reported for a job.
+ *                Internal - used in report_progress().
+ */
+CREATE OR REPLACE FUNCTION internal_create_task_table(in_table_name varchar(63))
+RETURNS VOID AS $$
+DECLARE
+  v_index_name VARCHAR(63);
+BEGIN
+
+  --  Raise exception if task table name has not been specified.
+  IF in_table_name IS NULL OR in_table_name = '' THEN
+    RAISE EXCEPTION 'Task table name has not been specified';
+  END IF;
+
+  --  Create a new task table.
+  EXECUTE format('
+  CREATE TABLE IF NOT EXISTS %I
+  (
+    task_id varchar(58) NOT NULL,
+    create_date timestamp without time zone NOT NULL,
+    status job_status NOT NULL DEFAULT ''Waiting''::job_status,
+    percentage_complete double precision NOT NULL DEFAULT 0.00,
+    failure_details text,
+    is_final boolean NOT NULL DEFAULT false,
+    CONSTRAINT %I PRIMARY KEY (task_id)
+  )', in_table_name, 'pk_' || in_table_name);
+
+  -- Create indexes.
+  v_index_name = 'idx_' || in_table_name || '_s';
+  IF (SELECT to_regclass(format('%I', v_index_name)::cstring)) IS NULL THEN
+    EXECUTE format('CREATE INDEX %1$I ON %2$I (%3$I)',v_index_name, in_table_name, 'status');
+  END IF;
+
+  v_index_name = 'idx_' || in_table_name || '_if';
+  IF (SELECT to_regclass(format('%I', v_index_name)::cstring)) IS NULL THEN
+    EXECUTE format('CREATE INDEX %1$I ON %2$I (%3$I)',v_index_name, in_table_name, 'is_final');
+  END IF;
+
+END
+$$ LANGUAGE plpgsql;
