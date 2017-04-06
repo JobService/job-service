47680d835d2fc87514ae3ff0280dcef805989c0a
smitcona <conal.smith@hpe.com>
2017-04-06 17:45:42 +0100

    Built job-service from commit 7c35da6 on branch gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,7 +38,7 @@ module.exports = function(grunt) {
             },
             pages: {
                 options: {
-                    remote: 'git@github.hpe.com:caf/job-service.git',
+                    remote: 'git@github.com:JobService/job-service.git',
                     login: '',
                     token: '',
                     branch: 'gh-pages'
