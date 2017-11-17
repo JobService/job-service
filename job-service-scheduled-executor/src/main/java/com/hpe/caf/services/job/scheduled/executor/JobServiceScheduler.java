f6bb83825183e2693d454339583da0881eb3aad9
Dermot Hardy <dermot.hardy@hpe.com>
2017-11-17 12:51:57 +0000

    Updated to wrap code into a worker

---
+++ job-service-scheduled-executor/src/main/java/com/hpe/caf/services/job/scheduled/executor/JobServiceScheduler.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015-2017 EntIT Software LLC, a Micro Focus company.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hpe.caf.services.job.scheduled.executor;
+
+import com.hpe.caf.worker.document.exceptions.DocumentWorkerTransientException;
+import com.hpe.caf.worker.document.extensibility.DocumentWorker;
+import com.hpe.caf.worker.document.model.Document;
+import com.hpe.caf.worker.document.model.HealthMonitor;
+
+public class JobServiceScheduler implements DocumentWorker
+{
+    public JobServiceScheduler()
+    {
+        ScheduledExecutor.main(null);
+    }
+
+    /**
+     * This method provides an opportunity for the worker to report if it has any problems which would prevent it processing documents
+     * correctly. If the worker is healthy then it should simply return without calling the health monitor.
+     *
+     * @param healthMonitor used to report the health of the application
+     */
+    @Override
+    public void checkHealth(HealthMonitor healthMonitor)
+    {
+    }
+
+    @Override
+    public void processDocument(Document document) throws InterruptedException, DocumentWorkerTransientException
+    {
+    }
+}
