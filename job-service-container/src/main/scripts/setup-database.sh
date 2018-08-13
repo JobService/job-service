5a2e4b256aa2e560dde7f5e06dd4d4d641861cd8
Om Mariappan <omkumar.mariappan@microfocus.com>
2018-08-13 08:44:39 -0700

    update copyright

--- job-service-container/src/main/scripts/setup-database.sh
+++ job-service-container/src/main/scripts/setup-database.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 #
-# Copyright 2017-2018 Micro Focus or one of its affiliates.
+# Copyright 2015-2018 Micro Focus or one of its affiliates.
 #
-# The only warranties for products and services of Micro Focus and its
-# affiliates and licensors ("Micro Focus") are set forth in the express
-# warranty statements accompanying such products and services. Nothing
-# herein should be construed as constituting an additional warranty.
-# Micro Focus shall not be liable for technical or editorial errors or
-# omissions contained herein. The information contained herein is subject
-# to change without notice.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Contains Confidential Information. Except as specifically indicated
-# otherwise, a valid license is required for possession, use or copying.
-# Consistent with FAR 12.211 and 12.212, Commercial Computer Software,
-# Computer Software Documentation, and Technical Data for Commercial
-# Items are licensed to the U.S. Government under vendor's standard
-# commercial license.
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 
