4f1ea53e99765a3927f3fe9e20cf4289a6f39a04
Anthony McGreevy <anthony.mcgreevy@microfocus.com>
2019-08-16 17:31:26 +0100

    SCMOD-7336: Admin caused failures now retried

--- worker-jobtracking/src/main/java/com/hpe/caf/worker/jobtracking/JobTrackingWorkerReporter.java
+++ worker-jobtracking/src/main/java/com/hpe/caf/worker/jobtracking/JobTrackingWorkerReporter.java
@@ -45,6 +45,8 @@ public class JobTrackingWorkerReporter implements JobTrackingReporter {
     private static final String FAILED_TO_REPORT_COMPLETION = "Failed to report the completion of job task {0}. {1}";
     private static final String FAILED_TO_REPORT_REJECTION = "Failed to report the failure and rejection of job task {0}. {1}";
 
+    private static final String POSTGRES_OPERATOR_FAILURE_CODE_PREFIX = "57";
+
     private static final Logger LOG = LoggerFactory.getLogger(JobTrackingWorkerReporter.class);
 
     @NotNull
@@ -143,6 +145,10 @@ public class JobTrackingWorkerReporter implements JobTrackingReporter {
             throw new JobReportingTransientException(
                 MessageFormat.format(FAILED_TO_REPORT_COMPLETION, jobTaskId, te.getMessage()), te);
         } catch (final SQLException se) {
+            if (isSqlStateIn(se, POSTGRES_OPERATOR_FAILURE_CODE_PREFIX)) {
+                throw new JobReportingTransientException(
+                    MessageFormat.format(FAILED_TO_REPORT_COMPLETION, jobTaskId, se.getMessage()), se);
+            }
             throw new JobReportingException(
                 MessageFormat.format(FAILED_TO_REPORT_COMPLETION, jobTaskId, se.getMessage()), se);
         }
