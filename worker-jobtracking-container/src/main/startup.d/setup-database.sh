2cc3fae0d2f2058c72be627c27c042eac6efb685
thilagasanthosh <thilagavathi.santhoshkumar@microfocus.com>
2020-01-23 14:05:47 +0000

    SCMOD-7554: Backward compatibility fix

--- worker-jobtracking-container/src/main/startup.d/setup-database.sh
+++ worker-jobtracking-container/src/main/startup.d/setup-database.sh
@@ -18,6 +18,6 @@
 
 cd /database
 java -jar job-service-db-installer.jar \
-     -db.user "${JOB_SERVICE_DATABASE_USERNAME}" \
-     -db.pass "${JOB_SERVICE_DATABASE_PASSWORD}" \
-     -db.connection.url "${JOB_SERVICE_DATABASE_URL}"
+     -db.user "${JOB_SERVICE_DATABASE_USERNAME:-${JOB_DATABASE_USERNAME}}" \
+     -db.pass "${JOB_SERVICE_DATABASE_PASSWORD:-${JOB_DATABASE_PASSWORD}}" \
+     -db.connection.url "${JOB_SERVICE_DATABASE_URL:-${JOB_DATABASE_URL}}"
