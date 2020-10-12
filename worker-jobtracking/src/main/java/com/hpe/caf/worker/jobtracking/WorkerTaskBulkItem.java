9bc0cbc8d20c08f19c7a98a9d04b29c29a45f4dd
Dermot Hardy <dermot.hardy@microfocus.com>
2020-10-12 13:54:44 +0100

    Add missing private keyword

--- worker-jobtracking/src/main/java/com/hpe/caf/worker/jobtracking/WorkerTaskBulkItem.java
+++ worker-jobtracking/src/main/java/com/hpe/caf/worker/jobtracking/WorkerTaskBulkItem.java
@@ -20,7 +20,7 @@ import javax.validation.constraints.NotNull;
 final class WorkerTaskBulkItem
 {
     @NotNull
-    final String jobId;
+    private final String jobId;
 
     @NotNull
     final private String partitionId;
