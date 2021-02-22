b11a7ec341db62da01a89fc9bb9b4f2b51c62a42
Dom Gibson <dominic.gibson@microfocus.com>
2021-02-22 13:41:57 +0000

    Use UNION over UNION ALL

--- job-service-db/src/main/resources/procedures/processFailedDependentJobs.sql
+++ job-service-db/src/main/resources/procedures/processFailedDependentJobs.sql
@@ -37,7 +37,7 @@ BEGIN
             SELECT partition_id, dependent_job_id, job_id
             FROM job_dependency
             WHERE partition_id = in_partition_id AND job_id = in_job_id
-            UNION ALL
+            UNION
             SELECT adj.partition_id, adj.dependent_job_id, jd.job_id
             FROM all_job_dependencies adj
             INNER JOIN job_dependency jd ON adj.partition_id = jd.partition_id AND adj.job_id = jd.dependent_job_id
