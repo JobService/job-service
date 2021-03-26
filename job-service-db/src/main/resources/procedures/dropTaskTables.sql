b6432b3dd38f5bfa1634a0a9f3c17c52c0a963c9
Simran <simranjeet.singhchawla@microfocus.com>
2021-03-26 12:20:11 +0530

    Removed unnecessary drop statement

--- job-service-db/src/main/resources/procedures/dropTaskTables.sql
+++ job-service-db/src/main/resources/procedures/dropTaskTables.sql
@@ -23,10 +23,6 @@
 DROP FUNCTION IF EXISTS internal_drop_task_tables(
     in_short_task_id VARCHAR(58)
 );
-DROP FUNCTION IF EXISTS internal_drop_task_tables(
-    in_partition_id VARCHAR(40),
-    in_task_id VARCHAR(58)
-);
 CREATE OR REPLACE FUNCTION internal_drop_task_tables(
     in_partition_id VARCHAR(40),
     in_task_id VARCHAR(58)
