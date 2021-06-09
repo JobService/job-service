64175a8356d90c56d347c21e2e7d18d9a38cae0d
Xbreizh <xavier.lamourec@gmail.com>
2021-06-09 15:51:40 +0100

    Removed in_propagate_failures checking

--- job-service-db/src/main/resources/procedures/reportFailure.sql
+++ job-service-db/src/main/resources/procedures/reportFailure.sql
@@ -82,8 +82,7 @@ BEGIN
     -- Update the task statuses in the tables
     PERFORM internal_report_task_status(in_partition_id, in_task_id, 'Failed', 0.00, in_failure_details);
 
-    IF in_propagate_failures THEN
-        PERFORM internal_process_failed_dependent_jobs(in_partition_id, v_job_id, in_failure_details);
-    END IF;
+    PERFORM internal_process_failed_dependent_jobs(in_partition_id, v_job_id, in_failure_details);
+
 END
 $$;
