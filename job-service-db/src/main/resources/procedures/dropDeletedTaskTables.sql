5072b53add0a46168e396705603cf4b511388513
Simran <simranjeet.singhchawla@microfocus.com>
2021-10-19 18:12:21 +0530

    formatting
    Signed-off-by: Simran <simranjeet.singhchawla@microfocus.com>

--- job-service-db/src/main/resources/procedures/dropDeletedTaskTables.sql
+++ job-service-db/src/main/resources/procedures/dropDeletedTaskTables.sql
@@ -38,15 +38,15 @@ BEGIN
     selected_parent_table_names :=
                     $q$SELECT table_name FROM deleted_parent_table_log LIMIT $q$ || commit_limit || $q$ FOR UPDATE SKIP LOCKED$q$;
     WHILE EXISTS(SELECT 1 FROM deleted_parent_table_log)
+    LOOP
+        FOR parent_table_log_rec IN EXECUTE selected_parent_table_names
         LOOP
-            FOR parent_table_log_rec IN EXECUTE selected_parent_table_names
-                LOOP
-                    CALL internal_populate_delete_log_table(parent_table_log_rec.table_name, 0);
-                    -- delete the parent table name from parent table.
-                    DELETE FROM deleted_parent_table_log WHERE table_name = parent_table_log_rec.table_name;
-                    COMMIT;
-                END LOOP;
+            CALL internal_populate_delete_log_table(parent_table_log_rec.table_name, 0);
+            -- delete the parent table name from parent table.
+            DELETE FROM deleted_parent_table_log WHERE table_name = parent_table_log_rec.table_name;
+            COMMIT;
         END LOOP;
+    END LOOP;
 
     selected_table_names := $q$SELECT table_name FROM delete_log LIMIT $q$ || commit_limit || $q$ FOR UPDATE SKIP LOCKED$q$;
 
