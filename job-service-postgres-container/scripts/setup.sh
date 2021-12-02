5487c44492206e68309a82b15f1e3938d661bd97
Xbreizh <xavier.lamourec@gmail.com>
2021-12-02 13:23:36 +0000

    restore

--- job-service-postgres-container/scripts/setup.sh
+++ job-service-postgres-container/scripts/setup.sh
@@ -19,8 +19,9 @@
 echo "Installing Job Service database."
 
 ./install_job_service_db.sh \
-  -db.user $POSTGRES_USER \
-  -db.pass $POSTGRES_PASSWORD \
+  -db.user ${POSTGRES_USER} \
+  -db.pass ${POSTGRES_PASSWORD} \
   -db.name ${POSTGRES_DB:-jobservice} \
   -db.connection jdbc:postgresql://127.0.0.1:5432/
+
 echo "Completed installation of Job Service database."
