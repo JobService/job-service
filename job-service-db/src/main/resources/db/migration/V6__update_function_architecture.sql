c46f42b7358fc8c2b1b329c3878adb1d5dcb9f6c
Xbreizh <xavier.lamourec@gmail.com>
2022-03-16 13:55:13 +0000

    drop functions

--- job-service-db/src/main/resources/db/migration/V6__update_function_architecture.sql
+++ job-service-db/src/main/resources/db/migration/V6__update_function_architecture.sql
@@ -31,53 +31,27 @@ $$
                 WHERE script LIKE 'procedures/R__%'
             )
         THEN
-            UPDATE flyway_schema_history AS fsh SET
-                script = c.script
-            FROM (VALUES
-                      ('cancelJob', 'functions/public/R__cancelJob.sql'),
-                      ('createJob', 'functions/public/R__createJob.sql'),
-                      ('createJobForDependencies', 'functions/public/R__createJobForDependencies.sql'),
-                      ('deleteDependentJob', 'functions/public/R__deleteDependentJob.sql'),
-                      ('deleteJob', 'functions/public/R__deleteJob.sql'),
-                      ('dropDeletedTaskTables', 'procedures/public/R__dropDeletedTaskTables.sql'),
-                      ('getDependentJobs', 'functions/public/R__getDependentJobs.sql'),
-                      ('getJob', 'functions/public/R__getJob.sql'),
-                      ('getJobs', 'functions/public/R__getJobs.sql'),
-                      ('getJobsCount', 'functions/public/R__getJobsCount.sql'),
-                      ('internal  cleanupCompletedSubtaskReport', 'functions/internal/R__internal__cleanupCompletedSubtaskReport.sql'),
-                      ('internal  createJob', 'functions/internal/R__internal__createJob.sql'),
-                      ('internal  createTaskTable', 'functions/internal/R__internal__createTaskTable.sql'),
-                      ('internal  doesTableExist', 'functions/internal/R__internal__doesTableExist.sql'),
-                      ('internal  dropTaskTables', 'functions/internal/R__internal__dropTaskTables.sql'),
-                      ('internal  getJobId', 'functions/internal/R__internal__getJobId.sql'),
-                      ('internal  getOptions', 'functions/internal/R__internal__getOptions.sql'),
-                      ('internal  getParentTaskId', 'functions/internal/R__internal__getParentTaskId.sql'),
-                      ('internal  getPrereqJobIdOptions', 'functions/internal/R__internal__getPrereqJobIdOptions.sql'),
-                      ('internal  getSubtaskCount', 'functions/internal/R__internal__getSubtaskCount.sql'),
-                      ('internal  getSubtaskId', 'functions/internal/R__internal__getSubtaskId.sql'),
-                      ('internal  getTaskStatus', 'functions/internal/R__internal__getTaskStatus.sql'),
-                      ('internal  getTaskTableName', 'functions/internal/R__internal__getTaskTableName.sql'),
-                      ('internal  hasDependentJobs', 'functions/internal/R__internal__hasDependentJobs.sql'),
-                      ('internal  insertDeleteLog', 'functions/internal/R__internal__insertDeleteLog.sql'),
-                      ('internal  insertParentTableToDelete', 'functions/internal/R__internal__insertParentTableToDelete.sql'),
-                      ('internal  isFinalTask', 'functions/internal/R__internal__isFinalTask.sql'),
-                      ('internal  isTaskCompleted', 'functions/internal/R__internal__isTaskCompleted.sql'),
-                      ('internal  populateDeleteLogTable', 'procedures/internal/R__internal__populateDeleteLogTable.sql'),
-                      ('internal  processDependentJobs', 'functions/internal/R__internal__processDependentJobs.sql'),
-                      ('internal  processFailedDependentJobs', 'functions/internal/R__internal__processFailedDependentJobs.sql'),
-                      ('internal  reportTaskStatus', 'functions/internal/R__internal__reportTaskStatus.sql'),
-                      ('internal  resolveStatus', 'functions/internal/R__internal__resolveStatus.sql'),
-                      ('internal  toRegClass', 'functions/internal/R__internal__toRegClass.sql'),
-                      ('internal  updateJobProgress', 'functions/internal/R__internal__updateJobProgress.sql'),
-                      ('internal  upsertIntoTaskTable', 'functions/internal/R__internal__upsertIntoTaskTable.sql'),
-                      ('pauseJob', 'functions/public/R__pauseJob.sql'),
-                      ('reportComplete', 'functions/public/R__reportComplete.sql'),
-                      ('reportCompleteBulk', 'functions/public/R__reportCompleteBulk.sql'),
-                      ('reportFailure', 'functions/public/R__reportFailure.sql'),
-                      ('reportProgress', 'functions/public/R__reportProgress.sql'),
-                      ('resumeJob', 'functions/public/R__resumeJob.sql')
-             ) AS c(description, script)
-            WHERE c.description = fsh.description;
+            DO
+            $$
+                DECLARE
+                    rec   record;
+                    rec_string varchar;
+                    function_drop varchar;
+
+                BEGIN
+                    FOR rec IN
+                        SELECT 'DROP FUNCTION ' || ns.nspname || '.' || proname
+                                   || '(' || oidvectortypes(proargtypes) || ');'
+                        FROM pg_proc INNER JOIN pg_namespace ns ON (pg_proc.pronamespace = ns.oid)
+                        WHERE ns.nspname = 'public'  order by proname
+                        LOOP
+
+                            rec_string = rec;
+                            select substring(rec_string, 3, length(rec_string) - 4) INTO function_drop;
+                            EXECUTE  function_drop;
+                        END LOOP;
+                END
+            $$;
 
         END IF;
 
