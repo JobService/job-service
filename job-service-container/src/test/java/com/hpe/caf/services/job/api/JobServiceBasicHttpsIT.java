341d3626691075d58a7be99fc3377cc7ebeff7fd
CORPDOM\MMcCallion <mark.mccallion@microfocus.com>
2023-03-29 16:02:24 +0100

    use partitions endpoint for ping

--- job-service-container/src/test/java/com/hpe/caf/services/job/api/JobServiceBasicHttpsIT.java
+++ job-service-container/src/test/java/com/hpe/caf/services/job/api/JobServiceBasicHttpsIT.java
@@ -106,6 +106,7 @@ public class JobServiceBasicHttpsIT
     public void basicCommunicationTest2() throws IOException, URISyntaxException
     {
         final URI getRequestUrl = UriBuilder.fromUri(https_url)
+            .path("partitions")
             .path("ping")
             .build();
         final HttpGet request = new HttpGet(getRequestUrl);
