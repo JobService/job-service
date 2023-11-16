ae4bb08d63381f23a1d6e1f5d640e8b832dbedae
Caoimhe.Lynch.76010228 <Caoimhe.Lynch@microfocus.com>
2023-11-16 13:20:30 +0000

    import

--- job-service-acceptance-tests/src/test/java/com/hpe/caf/jobservice/acceptance/IntegrationTestQueueServices.java
+++ job-service-acceptance-tests/src/test/java/com/hpe/caf/jobservice/acceptance/IntegrationTestQueueServices.java
@@ -40,6 +40,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeoutException;
 
